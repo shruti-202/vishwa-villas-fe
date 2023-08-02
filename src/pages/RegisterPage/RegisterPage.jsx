d4a605d1e1fdd96d987eb251b4eb32446f6a99ac
Shruti <shrutigawande1910@gmail.com>
2023-08-03 00:40:44 +0530

    chore: Fixed footer bottom

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import "./RegisterPage.css"
-import { FormControl,TextField,Button } from '@mui/material'
+import React from "react";
+import "./RegisterPage.css";
+import { FormControl, TextField, Button } from "@mui/material";
 
 function RegisterPage() {
   return (
@@ -8,28 +8,52 @@ function RegisterPage() {
       <div className="register-container">
         <div className="register-form">
           <h1>Sign Up</h1>
-        <FormControl fullWidth>
-        <TextField id="filled-basic" label="Name" variant="filled" required/>
-        <TextField id="filled-basic" label="Phone" variant="filled" type="number" required/>
-        <TextField id="filled-basic" label="Email" variant="filled" type="email" required/>
-        <TextField id="filled-basic" label="User Name" variant="filled" required/>
-        <TextField id="filled-basic" label="Password" variant="filled" type="password" required/>
-        <Button
+          <FormControl fullWidth>
+            <TextField
+              id="filled-basic"
+              label="Name"
+              variant="filled"
+              required
+            />
+            <TextField
+              id="filled-basic"
+              label="Phone"
+              variant="filled"
+              type="number"
+              required
+            />
+            <TextField
+              id="filled-basic"
+              label="Email"
+              variant="filled"
+              type="email"
+              required
+            />
+            <TextField
+              id="filled-basic"
+              label="User Name"
+              variant="filled"
+              required
+            />
+            <TextField
+              id="filled-basic"
+              label="Password"
+              variant="filled"
+              type="password"
+              required
+            />
+            <Button
               variant="contained"
               sx={{ marginTop: "20px", width: "100%" }}
               type="submit"
             >
               Sign Up
             </Button>
-      
-        </FormControl>
+          </FormControl>
         </div>
-        
-   
-       </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
