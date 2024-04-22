9016b0d819e161468e3d2a54f16f316c1dc968fe
Shruti <shrutigawande1910@gmail.com>
2024-04-22 11:25:42 +0530

    refactor: footer

---
+++ src/components/main/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p className="footer-text">
+          Made with ❤️ by <a href="https://shrutis-portfolio.netlify.app/" target="_blank"><u><i>Shruti Gawande</i></u> in IN</a>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
