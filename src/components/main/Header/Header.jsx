1a419bbb84a23894fc61e725dd430f95fa05d5e9
Shruti <shrutigawande1910@gmail.com>
2024-04-22 11:24:32 +0530

    feat: Header Section

---
+++ src/components/main/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import Logo from "../../../assets/logo.png";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <div className="header-left">
+          <Link to="/" className="header-left-brand">
+            <img src={Logo} alt="logo" />
+            <h2>Vishwa-Villas</h2>
+          </Link>
+        </div>
+        <div className="header-right">
+          <div className="header-right-login">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
