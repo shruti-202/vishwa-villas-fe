9885c2d811612518af115829bc946a1b4ec42f3c
Shruti <shrutigawande1910@gmail.com>
2024-05-13 12:55:09 +0530

    style: Styled the components

--- src/components/main/Footer/Footer.jsx
+++ src/components/main/Footer/Footer.jsx
@@ -6,10 +6,10 @@ function Footer() {
     <footer>
       <div className="footer-container">
         <p className="footer-text">
-          Made with ❤️ by <a href="https://shrutis-portfolio.netlify.app/" target="_blank"><u><i>Shruti Gawande</i></u> in IN</a>
+          Made with ❤️ by <a href="https://shrutis-portfolio.netlify.app/" target="_blank"><u><i>Shruti Gawande</i></u></a>  in IN
         </p>
       </div>
-    </footer>
+    </footer> 
   )
 }
 
