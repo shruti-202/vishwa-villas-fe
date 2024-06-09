b9bd0b04f3b25f888697c8713f7035b5a9269179
Shruti <shrutigawande1910@gmail.com>
2024-06-09 12:26:42 +0530

    feat: create add page

--- src/pages/CreateAddPage/CreateAddPage.jsx
+++ src/pages/CreateAddPage/CreateAddPage.jsx
@@ -1,12 +1,15 @@
 import React, { useRef, useState } from "react";
 import "./CreateAddPage.css";
 import { TextField, Button } from "@mui/material";
+import {v4 as uuidV4} from "uuid";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormLabel from "@mui/material/FormLabel";
 import { ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "../../../FirebaseConfig";
+import { errorAlert, successAlert} from "../../utility/alert";
+import { Navigate } from "react-router-dom";
 
 const descDefaultVal = `Type: House/Apartment/Condo/Villa/Other
 Bedrooms: 
@@ -24,12 +27,15 @@ Facing: North/East/West/South
 Project Name: `;
 
 function CreateAddPage() {
+  const [redirectToHome, setRedirectToHome] = useState(false)
   const [imagePaths, setImagePath] = useState([]);
-  const [files, setFiles] = useState(null);
+  const [files, setFiles] = useState([]);
   const title = useRef();
   const price = useRef();
   const location = useRef();
   const description = useRef();
+  const imgList = useRef([]);
+  const listType = useRef();
 
   const onImageChange = (event) => {
     if (event.target.files) {
@@ -43,11 +49,89 @@ function CreateAddPage() {
     }
   };
 
-  const submitAd = (event) => {
+  const submitAd = async (event) => {
     event.preventDefault();
-    const storageRef = ref(storage, `/ad-img/${files[0].name}`);
-    const upload = uploadBytesResumable(storageRef, files[0]);
+
+    const titleVal = title.current.value;
+    const priceVal = price.current.value;
+    const locationVal = location.current.value;
+    const descriptionVal = description.current.value;
+    const imgListVal = imgList.current;
+    const listTypeVal = listType.current;
+
+    if (!listTypeVal) {
+      errorAlert('Please select the list type', 'error')
+      return
+    }
+    if (!files.length) {
+      errorAlert('Please upload the images', 'error');
+      return;
+    }
+    if (!titleVal) {
+      errorAlert("Please enter the Title","error");
+      return;
+    }
+    if (titleVal.length < 5 || titleVal.length > 100) {
+      errorAlert("Title length should be greater than 5 and lesser than 100","error");
+      return;
+    }
+    if (!price) {
+      errorAlert("Please enter price","error");
+      return;
+    }
+    if (price < 0 || price > 1000000000) {
+      errorAlert("Price should be greater than 0 and less than 100,00,00,000", );
+      return;
+    }
+    if (descriptionVal.length > 1000) {
+      errorAlert('description length should be less than 1000 characters', 'error')
+      return
+    } 
+    if (files.length > 10) {
+      errorAlert("You can upload maximum 10 images", "error");
+      return;
+    }
+
+    //files upload
+    for(const file of files) {
+      const fileExt = file.name.split('.').pop();
+      const randomFileName = `${uuidV4()}.${fileExt}`;
+      const storageRef = ref(storage, `/ad-img/${randomFileName}`);
+      const upload = uploadBytesResumable(storageRef, files[0]);
+      imgList.current.push(`https://firebasestorage.googleapis.com/v0/b/vishwa-villas-b09e0.appspot.com/o/ad-img%2F${randomFileName}?alt=media&token=0a95dfd4-c4d1-44ae-a9d8-b9201d64d4bb`)
+    }
+
+    const response = await fetch(
+      `${import.meta.env.VITE_BASE_URL}/api/v1/item`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: titleVal,
+          price: priceVal,
+          location: locationVal,
+          description: descriptionVal,
+          imgList: imgListVal,
+          listType: listTypeVal
+        }),
+        credentials: "include",
+      }
+    );
+    const data = await response.json();
+    if (response.ok) {
+      successAlert(data.success, "success");
+      setRedirectToHome(true);
+     
+    } else {
+      errorAlert(data.error, "error");
+    }
   };
+
+  if (redirectToHome) {
+    return <Navigate to="/"/>
+  }
 
   return (
     <div className="create-add-page">
@@ -55,18 +139,7 @@ function CreateAddPage() {
         <div className="create-add-form">
           <h1>List My Property</h1>
           <form onSubmit={submitAd}>
-            <FormLabel id="demo-row-radio-buttons-group-label" required>
-              Listing Type
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel value="RENT" control={<Radio />} label="Rent" />
-              <FormControlLabel value="SELL" control={<Radio />} label="Sell" />
-            </RadioGroup>
-            <FormLabel
+          <FormLabel
               id="demo-row-radio-buttons-group-label"
               className="upload-images-label"
               required
@@ -81,7 +154,18 @@ function CreateAddPage() {
               accept="image/*"
               onChange={onImageChange}
             />
-
+            <FormLabel id="demo-row-radio-buttons-group-label" required>
+              Listing Type
+            </FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+              onChange={event => {listType.current = event.target.value }}
+            >
+              <FormControlLabel className="list-type" value="RENT" control={<Radio />} label="Rent" />
+              <FormControlLabel className="list-type" value="SELL" control={<Radio />} label="Sell" />
+            </RadioGroup>
             <TextField
               sx={{ marginBottom: "10px" }}
               fullWidth
@@ -113,7 +197,7 @@ function CreateAddPage() {
             <TextField
               sx={{ marginBottom: "10px" }}
               fullWidth
-              id="filled-multiline-static"
+              id="description"
               label="Description"
               multiline
               rows={5}
