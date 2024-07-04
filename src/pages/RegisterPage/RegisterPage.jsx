de9671b708accf651ab4238279ca634d7b52814d
Shruti <shrutigawande1910@gmail.com>
2024-07-04 12:45:20 +0530

    refactor

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -6,7 +6,7 @@ import { TextField, Button } from "@mui/material";
 import { Link, Navigate } from "react-router-dom";
 
 function RegisterPage() {
-  const [redirect, setRedirect] = useState(false)
+  const [redirect, setRedirect] = useState(false);
   const name = useRef();
   const phone = useRef();
   const email = useRef();
@@ -22,31 +22,34 @@ function RegisterPage() {
     const usernameVal = username.current.value;
     const passwordVal = password.current.value;
 
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/v1/auth/register`,{
-      method: 'POST',
-      headers :{
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify({
-        name: nameVal, 
-        phone: phoneVal, 
-        email: emailVal, 
-        username: usernameVal, 
-        password: passwordVal
-      })
-    })
+    const response = await fetch(
+      `${import.meta.env.VITE_BASE_URL}/api/v1/auth/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: nameVal,
+          phone: phoneVal,
+          email: emailVal,
+          username: usernameVal,
+          password: passwordVal,
+        }),
+      }
+    );
 
     const data = await response.json();
     if (response.ok) {
-      successAlert ('User Registered Successfully', 'success')
-      setRedirect(true)
+      successAlert("User Registered Successfully", "success");
+      setRedirect(true);
     } else {
-      errorAlert(data.error, 'error')
+      errorAlert(data.error, "error");
     }
   };
 
   if (redirect) {
-   return <Navigate to={"/login"}/>
+    return <Navigate to={"/login"} />;
   }
 
   return (
@@ -54,15 +57,15 @@ function RegisterPage() {
       <div className="register-container">
         <div className="register-form">
           <h1>SIGN UP</h1>
-          <form onSubmit={handleSubmit} >
+          <form onSubmit={handleSubmit}>
             <TextField
-            sx={{ marginBottom: "10px" }}
+              sx={{ marginBottom: "10px" }}
               fullWidth
               id="name"
               label="Name"
               variant="outlined"
-              autoComplete="true"
-              inputRef={name} 
+              autoComplete="off"
+              inputRef={name}
             />
             <TextField
               sx={{ marginBottom: "10px" }}
@@ -70,17 +73,17 @@ function RegisterPage() {
               id="email"
               label="Email"
               variant="outlined"
-              autoComplete="true"
+              autoComplete="off"
               inputRef={email}
             />
-              <TextField
-             sx={{ marginBottom: "10px" }}
+            <TextField
+              sx={{ marginBottom: "10px" }}
               fullWidth
               id="phone"
               label="Phone"
               variant="outlined"
               type="number"
-              autoComplete="true"
+              autoComplete="off"
               inputRef={phone}
             />
             <TextField
@@ -89,7 +92,7 @@ function RegisterPage() {
               id="username"
               label="User Name"
               variant="outlined"
-              autoComplete="true"
+              autoComplete="off"
               inputRef={username}
             />
             <TextField
@@ -99,26 +102,30 @@ function RegisterPage() {
               label="Password"
               variant="outlined"
               type="password"
-              autoComplete="true"
-              inputRef={password} 
+              autoComplete="off"
+              inputRef={password}
             />
             <Button
               variant="contained"
-              sx={{ marginTop: "10px", width: "100%" , backgroundColor: "var(--primary-color)",
-              color: "var(--ternary-color)",
-              "&:hover": {
-                backgroundColor: "var(--secondary-color)",
-              },}}
+              sx={{
+                marginTop: "10px",
+                width: "100%",
+                backgroundColor: "var(--primary-color)",
+                color: "var(--ternary-color)",
+                "&:hover": {
+                  backgroundColor: "var(--secondary-color)",
+                },
+              }}
               type="submit"
             >
               SIGN UP
             </Button>
-            <div style={{marginTop:"10px",textAlign:"center"}}>
-          Already have an account ? {""}
-          <Link to="/login" className="login-link">
-            <span style={{textDecoration:"underline"}}>LOGIN</span>
-          </Link>
-        </div>
+            <div style={{ marginTop: "10px", textAlign: "center" }}>
+              Already have an account ? {""}
+              <Link to="/login" className="login-link">
+                <span style={{ textDecoration: "underline" }}>LOGIN</span>
+              </Link>
+            </div>
           </form>
         </div>
       </div>
