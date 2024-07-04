89784fe9bba82a83e08e2ef59a75e272942455ff
Shruti <shrutigawande1910@gmail.com>
2024-07-04 12:37:33 +0530

    refactor: firebase storage for images

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -8,14 +8,14 @@ import {getStorage} from "firebase/storage"; // New Storage from firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvU01vjJ269ukxtg07KCEN6WDYe9dNduc",
-  authDomain: "vishwa-villas-b09e0.firebaseapp.com",
-  databaseURL: "https://vishwa-villas-b09e0-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "vishwa-villas-b09e0",
-  storageBucket: "vishwa-villas-b09e0.appspot.com",
-  messagingSenderId: "997311223686",
-  appId: "1:997311223686:web:4cf162d3c6de7060eebe07",
-  measurementId: "G-SMW39D87YM"
+  apiKey: "AIzaSyBcjUXHlx22Zqp9Pdl_L_dEuiH751F9PdU",
+  authDomain: "vishwa-villas-4ad0d.firebaseapp.com",
+  databaseURL: "https://vishwa-villas-4ad0d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "vishwa-villas-4ad0d",
+  storageBucket: "vishwa-villas-4ad0d.appspot.com",
+  messagingSenderId: "788143996265",
+  appId: "1:788143996265:web:78b42080b6e7067dd791f8",
+  measurementId: "G-WX814VDFJD"
 };
 
 // Initialize Firebase
