25b8ba2a23564b2f551bb15fad806c05e1c07188
Shruti <shrutigawande1910@gmail.com>
2024-07-13 14:35:39 +0530

    feat: footer

--- src/components/main/Footer/Footer.jsx
+++ src/components/main/Footer/Footer.jsx
@@ -1,16 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+
+const footerNavLink = [
+  {
+    link: "/",
+    icon: HomeOutlinedIcon,
+    color: "var(--secondary-color)",
+    activeColor: "var(--primary-color)",
+  },
+  {
+    link: "/create",
+    icon: AddCircleOutlinedIcon,
+    color: "var(--secondary-color)",
+    activeColor: "var(--primary-color)",
+  },
+  {
+    link: "/profile",
+
+    icon: PersonOutlineOutlinedIcon,
+    color: "var(--secondary-color)",
+    activeColor: "var(--primary-color)",
+  },
+];
 
 function Footer() {
+  const [activePath, setActivePath] = useState();
+
   return (
-    <footer>
+    <>
+    <div className="footer-pad"></div>
+     <footer>
       <div className="footer-container">
-        <p className="footer-text">
-          Made with ❤️ by <a href="https://shrutis-portfolio.netlify.app/" target="_blank"><u><i>Shruti Gawande</i></u></a>  in IN
-        </p>
+        {footerNavLink.map((navLink, index) => (
+          <FooterNavLink
+            key={index}
+            Icon={navLink.icon}
+            link={navLink.link}
+            color={navLink.color}
+            activeColor={navLink.activeColor}
+            activePath={activePath}
+            setActivePath={setActivePath}
+          />
+        ))}
       </div>
-    </footer> 
-  )
+    </footer>
+    </>
+  );
 }
 
-export default Footer
+const FooterNavLink = ({
+  Icon,
+  link,
+  color,
+  activeColor,
+  activePath,
+  setActivePath,
+}) => {
+  return (
+    <div className="footer-nav-link">
+      <Link to={link} onClick={() => setActivePath(link)}>
+        <Icon style={{ color: activePath === link ? activeColor : color }} />
+      </Link>
+    </div>
+  );
+};
+
+export default Footer;
